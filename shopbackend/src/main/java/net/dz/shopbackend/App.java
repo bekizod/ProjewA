17cf38c0ad350c7b96060fb64aabc9012ef25958
bekizod <121251335+bekizod@users.noreply.github.com>
2024-03-14 10:04:53 +0300

    first commit

---
+++ shopbackend/src/main/java/net/dz/shopbackend/App.java
@@ -0,0 +1,22 @@
+package net.dz.shopbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+public void firsMethod(String name, String  Id){
+
+}
+public void secondMethod ()  {
+}
+
+public void bkMethod(){
+}
+}
